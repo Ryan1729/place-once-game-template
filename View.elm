2cb52cfbf74f3c62bc3bbeace228c686728d6e00
Ryan1729 <Ryan1729@gmail.com>
2016-11-09 06:35:09 -0700

    add common view portion
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- View.elm
+++ View.elm
@@ -1,20 +1,24 @@
 module View exposing (view)
 
 import MaterialModel exposing (MaterialModel)
-import Model exposing (Model)
+import Model exposing (Model, Piece, Board, GameState(..))
 import Html exposing (Html, text)
+import Html.App
+import Html.Attributes
 import MaterialMsg exposing (MaterialMsg(Mdl, U))
 import Msg exposing (Msg(..))
 import Material.Button as Button
+import Material.Grid as Grid exposing (Device(..))
+import Svg exposing (Svg, svg, rect, path, Attribute, ellipse, g)
+import Svg.Attributes exposing (..)
+import Svg.Events exposing (onClick)
+import PieceView
 
 
 view : MaterialModel -> Html MaterialMsg
 view { mdl, model } =
     Html.div []
-        [ model
-            |> toString
-            |> text
-        , Button.render Mdl
+        [ Button.render Mdl
             [ 0 ]
             mdl
             [ Button.raised
@@ -22,4 +26,66 @@ view { mdl, model } =
             , Button.onClick (U NewGame)
             ]
             [ text "New Game" ]
+        , Grid.grid []
+            [ Grid.cell [ Grid.size All 5 ]
+                [ PieceView.renderRack model.selected model.rack
+                ]
+            , Grid.cell [ Grid.size All 6 ]
+                [ Html.div [ Html.Attributes.style [ ( "width", boardWidthString ++ "px" ), ( "display", "flex" ), ( "justify-content", "center" ), ( "font-size", (boardWidth / 32 |> toString) ++ "px" ) ] ]
+                    [ model.gameState
+                        |> gameStateToString
+                        |> Html.text
+                    ]
+                , svg
+                    [ width boardWidthString
+                    , height boardHeightString
+                    , viewBox ("0 0 " ++ boardWidthString ++ " " ++ boardHeightString)
+                    ]
+                    [ renderBoard model.selected model.board
+                    ]
+                ]
+            ]
+            |> Html.App.map U
         ]
+
+
+gameStateToString : GameState -> String
+gameStateToString gameState =
+    case gameState of
+        Win ->
+            "You won!"
+
+        Loss ->
+            "You lost!"
+
+        _ ->
+            ""
+
+
+renderBoard : Maybe Piece -> Board -> Svg Msg
+renderBoard selected board =
+    Svg.text "board"
+
+
+boardWidth =
+    720
+
+
+boardWidthString =
+    toString boardWidth
+
+
+boardHeight =
+    720
+
+
+boardHeightString =
+    toString boardHeight
+
+
+centerX =
+    boardWidth / 2
+
+
+centerY =
+    boardHeight / 2
